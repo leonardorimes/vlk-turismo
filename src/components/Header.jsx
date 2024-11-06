74c69d743e767958bf7e1b73cdba6c5a991afe61
Leonardo Rimes <rimesleo@gmail.com>
2024-11-06 10:23:29 -0300

    finzalizando home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,11 @@ import NavBar from "./NavBar";
 
 function Header({ menu, setCloseMenu, handleMenu }) {
   return (
-    <header>
+    <header className="w-[80%] mx-auto my-0">
       <img
         src="../public/VLK.png"
         alt="logomarca VLK"
-        className="w-56 absolute top-10 left-4 "
+        className="w-56 absolute top-10 left-15 "
       />
       <NavBar menu={menu} setCloseMenu={setCloseMenu} handleMenu={handleMenu} />
     </header>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,13 +13,37 @@ function Home() {
 
   return (
     <div className="bg-[url('/public/BACKGROUND.png')] bg-no-repeat bg-cover ">
-      <Header menu={menu} setCloseMenu={setCloseMenu} handleMenu={handleMenu} />
-      <div className="sm:hidden md:block search flex justify-center mx-auto mt-20 gap-9 relative w-[30%] ">
-        <Search className=" w-full h-14 rounded-xl shadow-lg outline-none p-8" />
+      <div className="bg-cover w-[80%] mx-auto my-0">
+        <Header
+          menu={menu}
+          setCloseMenu={setCloseMenu}
+          handleMenu={handleMenu}
+        />
+        <div className="sm:hidden md:block search flex justify-center mx-auto mt-20 gap-9 relative w-[30%] ">
+          <Search className=" w-full h-14 rounded-xl shadow-lg outline-none p-8" />
 
-        <Button className="  rounded-full bg-green-700 absolute  right-2  top-3 px-10 py-2 text-white flex items-center gap-2">
-          <IoSearch /> Buscar
-        </Button>
+          <Button className="  rounded-full bg-green-700 absolute  right-2  top-3 px-10 py-2 text-white flex items-center gap-2">
+            <IoSearch /> Buscar
+          </Button>
+        </div>
+        <div className="flex sm:flex-col-reverse lg:flex-row sm:items-center  justify-center gap-24 mt-28 w-full">
+          <div className="w-[450px]">
+            <h1 className="text-5xl font-bold mb-6">
+              Descubra o mundo com a VLK!
+            </h1>
+            <p className="mb-6">
+              Nossa agência de turismo oferece experiências personalizadas e
+              roteiros cuidadosamente planejados para transformar suas viagens
+              em memórias inesquecíveis.
+            </p>
+
+            <Button className="rounded-full bg-green-700 px-12 py-4 text-white font-bold">
+              {" "}
+              Explorar Destinos
+            </Button>
+          </div>
+          <img src="../public/IMG-HOME.png" alt="" />
+        </div>
       </div>
     </div>
   );
