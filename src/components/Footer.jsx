7eab331bf75bcaa0238cf0a63941e633b286f999
Leonardo Rimes <rimesleo@gmail.com>
2024-11-16 20:57:53 -0300

    acertos na versao mobile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,16 +3,12 @@ import { FaFacebook } from "react-icons/fa";
 
 function Footer() {
   return (
-    <footer className="bg-slate-200 p-2 mt-20   gap-96 ">
-      <div className="flex justify-center items-center lg:gap-[800px] md:flex-col sm:flex-col lg:flex-row md:gap-10 sm:gap-10">
+    <footer className="bg-slate-200 p-2 mt-20  ">
+      <div className="flex justify-center items-center xs:gap-32 lg:gap-[800px] md:flex-col sm:flex-col lg:flex-row md:gap-10 sm:gap-10">
         <div className="img  ">
-          <img
-            src="../public/VLK.png"
-            alt="logomarca VLK"
-            className="w-full w-28"
-          />
+          <img src="../public/VLK.png" alt="logomarca VLK" className=" w-28" />
         </div>
-        <div className="social flex gap-10">
+        <div className="social flex gap-10 ">
           <a href="https://www.instagram.com/vlkturismo/" target="_blank">
             <AiFillInstagram className="text-green-900 text-2xl" />
           </a>
