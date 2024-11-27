897718d83be38c83e20d8c49a5be30502b11f052
Leonardo Rimes <rimesleo@gmail.com>
2024-11-27 10:47:10 -0300

    acrescentando caminho na logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,13 @@
 import { AiFillInstagram } from "react-icons/ai";
 import { FaFacebook } from "react-icons/fa";
+import logo from "../assets/images/VLK.png";
 
 function Footer() {
   return (
     <footer className="bg-slate-200 p-2 mt-20  ">
       <div className="flex justify-center items-center xs:gap-32 lg:gap-[800px] md:flex-col sm:flex-col lg:flex-row md:gap-10 sm:gap-10">
         <div className="img  ">
-          <img src="./public/VLK.png" alt="logomarca VLK" className=" w-28" />
+          <img src={logo} alt="logomarca VLK" className=" w-28" />
         </div>
         <div className="social flex gap-10 ">
           <a href="https://www.instagram.com/vlkturismo/" target="_blank">
